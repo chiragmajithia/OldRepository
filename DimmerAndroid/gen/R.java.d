695bb27d248339c82461eab1fdbed2371c72725a
Chirag Majithia <chiragmajithia@users.noreply.github.com>
2018-01-28 13:21:45 -0800

    DimmerAndroid

---
+++ DimmerAndroid/gen/R.java.d
@@ -0,0 +1,12 @@
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\gen\com\Dimmer\Android\R.java \
+ : D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\res\drawable-hdpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\res\drawable-ldpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\res\drawable-mdpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\res\drawable-xhdpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\res\layout\main_activity.xml \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\res\values\strings.xml \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\bin\res\drawable-hdpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\bin\res\drawable-ldpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\bin\res\drawable-mdpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\bin\res\drawable-xhdpi\ic_launcher.png \
+D:\Users\XPS\Documents\NetBeansProjects\DimmerAndroid\bin\AndroidManifest.xml \
